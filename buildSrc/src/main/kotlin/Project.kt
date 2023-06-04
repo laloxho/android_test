9a408531b21d6d62c2ca569e8ae2c385646a38d3
Eduardo Zamorano <jezamorano_0606@outlook.com>
2023-06-04 17:28:01 -0600

    Update version

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -2,6 +2,6 @@ object Project {
     const val compileSdk = 33
     const val minSdk = 21
     const val targetSdk = 33
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
 }
